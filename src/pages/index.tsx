162b26cdf7f5ce2efbeb0db47a8d171d7b416824
Max Clinton <maxtclinton@gmail.com>
2023-01-24 22:01:42 -0500

    eslint cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { type NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import className from "classnames";
-import { UserIcon } from '@heroicons/react/24/solid'
 import {
     ArrowPathRoundedSquareIcon,
     ChatBubbleLeftIcon,
     HeartIcon,
 } from "@heroicons/react/24/outline";
+import { UserIcon } from '@heroicons/react/24/solid'
+import className from "classnames";
+import { type NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 import { api } from "../utils/api";
 
